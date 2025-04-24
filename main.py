e83563a3a4ec7a742fe9c54264470144a22d41ef
chandan <c.jha@straiv.io>
2025-04-24 16:45:05 +0200

    trainer evaluation and mlflow committed

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 import sys
 
 
@@ -29,5 +30,10 @@ if __name__=='__main__':
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         logging.info('Complete Data transformation')
         print(data_transformation_artifact)
+        logging.info('Model Training started')
+        model_trainer_config=ModelTrainerConfig(training_pipeline_config)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info('Model Training completed')
     except Exception as e:
         raise NetworkSecurityException(e,sys)
