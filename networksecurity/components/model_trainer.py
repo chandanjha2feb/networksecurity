d0da7ba148e02f3058d8c96b9fa4c8f202793ceb
chandan <c.jha@straiv.io>
2025-04-25 10:36:00 +0200

    Create pipeline

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -25,6 +25,10 @@ from sklearn.ensemble import (
 )
 import mlflow
 
+import dagshub
+dagshub.init(repo_owner='chandanjha2feb', repo_name='networksecurity', mlflow=True)
+
+
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
         try:
@@ -125,8 +129,9 @@ class ModelTrainer:
 
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=NetworkModel)
-        #model pusher
-        #save_object("final_model/model.pkl",best_model)
+        
+        # model pusher
+        save_object("final_model/model.pkl",best_model)
         
 
         ## Model Trainer Artifact
